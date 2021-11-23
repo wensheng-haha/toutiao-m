f0b78efe77659a646967a23184b189c75c46c4d0
wengsheng <1666944880@qq.com>
2021-11-23 16:18:17 +0800

    项目初始化完成

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,16 @@
   <div>
     <h1>黑马头条</h1>
     <router-view/>
+    <div>
+      <i class="toutiao toutiao-shouye"></i>
+    </div>
+    <div>
+      <van-button type="primary">主要按钮</van-button>
+      <van-button type="info">信息按钮</van-button>
+      <van-button type="default">默认按钮</van-button>
+      <van-button type="warning">警告按钮</van-button>
+      <van-button type="danger">危险按钮</van-button>
+    </div>
   </div>
 </template>
 
